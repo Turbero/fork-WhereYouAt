8096182868dafb87142826fb2662f9345c842c12
AzumattDev <azumattgaming@gmail.com>
2022-10-23 21:20:08 -0400

    1.0.4

--- WhereYouAt.cs
+++ WhereYouAt.cs
@@ -13,7 +13,7 @@ namespace WhereYouAt
 
     {
         internal const string ModName = "WhereYouAt";
-        internal const string ModVersion = "1.0.3";
+        internal const string ModVersion = "1.0.4";
         internal const string Author = "Azumatt";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
